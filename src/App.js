a7b822901783ddabb2d9db81d658123ec92be9c1
xaviguasch <xaviguasch@gmail.com>
2019-06-04 11:03:28 +0200

    toyed with the reducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { useReducer } from 'react'
 import './App.css'
 
-function reducer(state, action) {
-  if (action.type === 'INCREMENT') return { count: state.count + 1 }
-  if (action.type === 'DECREMENT') return { count: state.count - 1 }
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.amount }
+
+    case 'DECREMENT':
+      return { count: state.count - action.amount }
+    case 'RESET':
+      return { count: (state.count = 0) }
+  }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const [state, dispatch] = useReducer(countReducer, { count: 0 })
 
   return (
     <div className='App'>
       <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Add 1</button>
-      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Subtract 1</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT', amount: 1 })}>Add 1</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT', amount: 5 })}>Add 5</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', amount: 1 })}>
+        Subtract 1{' '}
+      </button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
     </div>
   )
 }
