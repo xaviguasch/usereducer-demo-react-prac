89fc558f74fa7a65f9f68d198351821982bc10e9
xaviguasch <xaviguasch@gmail.com>
2019-06-04 10:22:52 +0200

    added simple reducer demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer(state, action) {
+  if (action.type === 'INCREMENT') return { count: state.count + 1 }
+  if (action.type === 'DECREMENT') return { count: state.count - 1 }
 }
 
-export default App;
+function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  return (
+    <div className='App'>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Add 1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Subtract 1</button>
+    </div>
+  )
+}
+
+export default App
